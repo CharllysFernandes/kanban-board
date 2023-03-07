b8b91565b957c83e9680c1a5ca248e4d0f2da36d
Charllys Fernandes <charllys.daiany@gmail.com>
2023-03-07 17:22:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,54 +1,54 @@
-const tarefas = []
+const tasks = []
 
 // Seleciona todos os botões com ID "adicionarTarefa"
 const botoes = document.querySelectorAll('#adicionarTarefa');
 
 // Função para salvar o JSON no localStorage
-function salvarTarefasNoLocalStorage(tarefas) {
-    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+function saveTasksToLocalStorage(tasks) {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
 // Função para carregar o JSON do localStorage
-function carregarTarefasDoLocalStorage() {
-    const tarefasJSON = localStorage.getItem('tarefas');
-    if (tarefasJSON === null) {
+function loadTasksFromLocalStorage() {
+    const tasksJSON = localStorage.getItem('tasks');
+    if (tasksJSON === null) {
         return [];
     } else {
-        return JSON.parse(tarefasJSON);
+        return JSON.parse(tasksJSON);
     }
 }
 
 // Adiciona um evento de clique a cada botão
 
 
-botoes.forEach(botao => {
-    botao.addEventListener('click', () => {
+botoes.forEach(button => {
+    button.addEventListener('click', () => {
         // Encontra o id do elemento pai do elemento pai do botão
-        const column = botao.parentElement.parentElement;
+        const column = button.parentElement.parentElement;
         const status = column.id.slice(7); // remove o "column-" do id encontrado.
 
         // Cria um novo elemento div com a classe "cardTask rounded-2 shadow-sm"
-        const novaDiv = document.createElement('div');
-        novaDiv.classList.add('cardTask', 'rounded-2', 'shadow-sm');
+        const newDiv = document.createElement('div');
+        newDiv.classList.add('cardTask', 'rounded-2', 'shadow-sm');
 
         // Adiciona o conteúdo da div, incluindo o texto e o botão com o ícone
-        novaDiv.innerHTML = `
+        newDiv.innerHTML = `
           <span class="me-2">Empty the little box</span>
           <button id="excluirTarefa" class="btn border-0"><i class="bi bi-x-circle-fill"></i></button>
         `;
 
         // Adiciona a div como um filho do elemento pai do elemento pai do botão
-        column.appendChild(novaDiv);
+        column.appendChild(newDiv);
 
-        const novaTarefa = {
+        const newTask = {
             task: "Empty the little box",
             status,
         }
 
-        tarefas.push(novaTarefa);
+        tasks.push(newTask);
 
-        novaDiv.querySelector('#excluirTarefa').addEventListener('click', () => {
-            novaDiv.remove();
+        newDiv.querySelector('#excluirTarefa').addEventListener('click', () => {
+            newDiv.remove();
         });
     });
 });

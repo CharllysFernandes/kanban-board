b4f12be2e93a19f47e5f36a1c69e9370b1dfce4f
Charllys Fernandes <charllys.daiany@gmail.com>
2023-03-08 00:00:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-const tasks = []
 
 // Seleciona todos os botões com ID "adicionarTarefa"
 const botoes = document.querySelectorAll('#adicionarTarefa');
@@ -17,6 +16,8 @@ function loadTasksFromLocalStorage() {
         return JSON.parse(tasksJSON);
     }
 }
+
+const tasks = loadTasksFromLocalStorage();
 
 // Adiciona um evento de clique a cada botão
 
@@ -40,12 +41,16 @@ botoes.forEach(button => {
         // Adiciona a div como um filho do elemento pai do elemento pai do botão
         column.appendChild(newDiv);
 
+
         const newTask = {
             task: "Empty the little box",
             status,
         }
 
+
         tasks.push(newTask);
+
+        saveTasksToLocalStorage(tasks)
 
         newDiv.querySelector('#excluirTarefa').addEventListener('click', () => {
             newDiv.remove();
@@ -53,3 +58,63 @@ botoes.forEach(button => {
     });
 });
 
+function distributeTasksByColumnFromLocalStorage() {
+    const tasks = loadTasksFromLocalStorage();
+    const tasksNoSpecified = tasks.filter(task => task.status === 'Not-Specified');
+    const tasksToDo = tasks.filter(task => task.status === 'To-Do');
+    const tasksInProgress = tasks.filter(task => task.status === 'In-Progress');
+    const tasksCompleted = tasks.filter(task => task.status === 'Completed');
+
+    
+    return [tasksNoSpecified, tasksToDo, tasksInProgress, tasksCompleted];
+  }
+
+// Distribui as tarefas do localStorage por coluna
+const [tasksNoSpecified, tasksToDo, tasksInProgress, tasksCompleted] = distributeTasksByColumnFromLocalStorage();
+
+// Adiciona as tarefas à lista de tarefas correspondente a cada coluna
+const notSpecifiedList = document.querySelector('#column-Not-Specified');
+tasksNoSpecified.forEach(task => {
+  const newDiv = createTaskCard(task);
+  notSpecifiedList.appendChild(newDiv);
+});
+
+const tasksToDoList = document.querySelector('#column-To-Do');
+tasksToDo.forEach(task => {
+    const newDiv = createTaskCard(task);
+    tasksToDoList.appendChild(newDiv);
+})
+
+const tasksInProgressList = document.querySelector('#column-In-Progress');
+tasksInProgress.forEach(task => {
+  const newDiv = createTaskCard(task);
+  tasksInProgressList.appendChild(newDiv);
+});
+
+const tasksCompletedList = document.querySelector('#column-Completed');
+tasksCompleted.forEach(task => {
+  const newDiv = createTaskCard(task);
+  tasksCompletedList.appendChild(newDiv);
+});
+
+
+function createTaskCard(task) {
+    // Cria um novo elemento div com a classe "cardTask rounded-2 shadow-sm"
+    const newDiv = document.createElement('div');
+    newDiv.classList.add('cardTask', 'rounded-2', 'shadow-sm');
+  
+    // Adiciona o conteúdo da div, incluindo o texto e o botão com o ícone
+    newDiv.innerHTML = `
+      <span class="me-2">${task.task}</span>
+      <button id="excluirTarefa" class="btn border-0"><i class="bi bi-x-circle-fill"></i></button>
+    `;
+  
+    // Adiciona um evento de clique ao botão de exclusão para remover a tarefa da lista e do localStorage
+    newDiv.querySelector('#excluirTarefa').addEventListener('click', () => {
+      newDiv.remove(); // remove a div da lista de tarefas
+      //removeTaskFromLocalStorage(taskId); // remove a tarefa correspondente do localStorage
+    });
+  
+    return newDiv;
+  }
+  

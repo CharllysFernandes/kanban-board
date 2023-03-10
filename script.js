5c3310ac698fe4b5ab23b2972ea6e2e4a2ba8607
Charllys Fernandes <charllys.daiany@gmail.com>
2023-03-10 00:37:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,59 +1,38 @@
 
-// Seleciona todos os botões com ID "adicionarTarefa"
 const botoes = document.querySelectorAll('#adicionarTarefa');
-
-// Função para salvar o JSON no localStorage
-function saveTasksToLocalStorage(tasks) {
-  localStorage.setItem('tasks', JSON.stringify(tasks));
-}
-
-// Função para carregar o JSON do localStorage
-function loadTasksFromLocalStorage() {
-  const tasksJSON = localStorage.getItem('tasks');
-  if (tasksJSON === null) {
-    return [];
-  } else {
-    return JSON.parse(tasksJSON);
-  }
-}
-
 const tasks = loadTasksFromLocalStorage();
-
-// Adiciona um evento de clique a cada botão
-
 
 botoes.forEach(button => {
   button.addEventListener('click', () => {
-    // Encontra o id do elemento pai do elemento pai do botão
     const column = button.parentElement.parentElement;
     const status = column.id.slice(7); // remove o "column-" do id encontrado.
-
-    // Cria um novo elemento div com a classe "cardTask rounded-2 shadow-sm"
     const newDiv = document.createElement('div');
     newDiv.classList.add('cardTask', 'rounded-2', 'shadow-sm');
+    newDiv.setAttribute('id', 'cardTask');
 
-    // Adiciona o conteúdo da div, incluindo o texto e o botão com o ícone
     newDiv.innerHTML = `
           <span class="me-2">Empty the little box</span>
           <button id="excluirTarefa" class="btn border-0"><i class="bi bi-x-circle-fill"></i></button>
         `;
 
-    // Adiciona a div como um filho do elemento pai do elemento pai do botão
     column.appendChild(newDiv);
-
 
     const newTask = {
       task: "Empty the little box",
       status,
     }
 
-
     tasks.push(newTask);
 
     saveTasksToLocalStorage(tasks)
 
     newDiv.querySelector('#excluirTarefa').addEventListener('click', () => {
+      window.location.reload()
       newDiv.remove();
+
+      const spanText = newDiv.querySelector('span').textContent;
+      excluirDoLocalStorage(spanText);
+
     });
   });
 });
@@ -113,50 +92,61 @@ function createTaskCard(task) {
 
   // Adiciona um evento de clique ao botão de exclusão para remover a tarefa da lista e do localStorage
   newDiv.querySelector('#excluirTarefa').addEventListener('click', () => {
-    newDiv.remove(); // remove a div da lista de tarefas
+    newDiv.remove();
 
-    // TODO
+    const spanText = newDiv.querySelector('span').textContent;
+    excluirDoLocalStorage(spanText);
 
-    
+
   });
 
   return newDiv;
 }
 
 
-  const taskCards = document.querySelectorAll('#cardTask');
-  taskCards.forEach(card => {
-    const taskText = card.querySelector('span');
-    taskText.addEventListener('click', () => {
+const taskCards = document.querySelectorAll('#cardTask');
+taskCards.forEach(card => {
+  const taskText = card.querySelector('span');
+  taskText.addEventListener('click', () => {
 
-      const tasks = loadTasksFromLocalStorage();
-      const oldText = taskText.textContent;
-      const newText = prompt('Atualize o texto da tarefa', taskText.textContent);
+    const tasks = loadTasksFromLocalStorage();
+    const oldText = taskText.textContent;
+    const newText = prompt('Atualize o texto da tarefa', taskText.textContent);
 
-      if (newText !== null) {
-        taskText.textContent = newText;
+    if (newText !== null) {
+      taskText.textContent = newText;
 
-        // Localiza o objeto correspondente no array tasks
-        const taskToUpdate = tasks.find(task => task.task === oldText);
+      // Localiza o objeto correspondente no array tasks
+      const taskToUpdate = tasks.find(task => task.task === oldText);
 
-        if (taskToUpdate) {
-          // Atualiza a propriedade task com o novo valor
-          taskToUpdate.task = newText;
+      if (taskToUpdate) {
+        // Atualiza a propriedade task com o novo valor
+        taskToUpdate.task = newText;
 
-          // Salva o array atualizado de volta no localStorage
-          saveTasksToLocalStorage(tasks);
-        }
+        // Salva o array atualizado de volta no localStorage
+        saveTasksToLocalStorage(tasks);
       }
+    }
 
 
-    });
   });
+});
 
-  function getTextoDoSpanDoBotaoExcluir(botaoExcluir) {
-    const span = botaoExcluir.parentNode.querySelector('span');
-    return span.textContent;
+
+function excluirDoLocalStorage(texto) {
+  const index = tasks.findIndex(task => task.task === texto);
+
+  if (index !== -1) {
+    tasks.splice(index, 1);
+    saveTasksToLocalStorage(tasks)
   }
+}
 
-  const botaoExcluir = document.querySelector('#excluirTarefa');
-const textoDoSpan = getTextoDoSpanDoBotaoExcluir(botaoExcluir);
+function saveTasksToLocalStorage(tasks) {
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
 
+function loadTasksFromLocalStorage() {
+  const tasksJSON = localStorage.getItem('tasks');
+  return tasksJSON ? JSON.parse(tasksJSON) : [];
+}

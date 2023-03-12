a33f7127f182d20d0df530bdc88815d259bd2d4e
Charllys Fernandes <charllys.daiany@gmail.com>
2023-03-12 00:14:23 -0300

    upgrade index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const COLUMN_NOT_ESPECIFIED = document.getElementById('not-Specified');
-const COLUMN_TO_DO = document.getElementById('To-Do');
+const COLUMN_TO_DO = document.getElementById('toDo');
 const COLUMN_IN_PROGRESS = document.getElementById('in-Progress');
 const COLUMN_COMPLETED = document.getElementById('completed');
 
@@ -32,6 +32,7 @@ function addTask(column) {
     taskList.push(newTask)
     saveTasksToLocalStorage(taskList);
     loadTaskInColumn(column, uniqueID)
+    console.log(column)
 
 }
 
@@ -40,17 +41,39 @@ function loadTaskInColumn(column, idTask) {
 
     console.log(column, idTask)
     const columnDiv = document.getElementById(column);
+    console.log(columnDiv)
     const matchingTask = taskList.find(task => task.id === idTask);
 
+    console.log(matchingTask)
+
+    const newDiv = createTaskCard(matchingTask);
+    columnDiv.appendChild(newDiv);
+
+    // const newDiv = document.createElement('div');
+    // newDiv.classList.add('card-task', 'rounded-2', 'shadow-sm');
+    // newDiv.setAttribute('id', 'cardTask');
+    // newDiv.innerHTML = `
+    // <span class="me-2">${matchingTask.task}</span>
+    // <button onclick="deleteTask(${idTask})" class="btn border-0"><i class="bi bi-x-circle-fill"></i></button>
+    // `;
+
+    // columnDiv.innerHTML += newDiv;
+
+    // return newDiv;
+
+}
+
+
+
+function createTaskCard(task) {
     const newDiv = document.createElement('div');
     newDiv.classList.add('card-task', 'rounded-2', 'shadow-sm');
     newDiv.setAttribute('id', 'cardTask');
+    
     newDiv.innerHTML = `
-    <span class="me-2">${matchingTask.task}</span>
-    <button onclick="deleteTask(${idTask})" class="btn border-0"><i class="bi bi-x-circle-fill"></i></button>
-    `;
-
-    columnDiv.innerHTML += newDiv;
-
-
-}
+        <span class="me-2">${task.task}</span>
+        <button onclick="deleteTask(${task.id})" class="btn border-0"><i class="bi bi-x-circle-fill"></i></button>
+      `;
+      
+    return newDiv;
+  }

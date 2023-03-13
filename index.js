5ec5ea825fd17388e967054fd083bdaf67ec5519
Charllys Fernandes <charllys.daiany@gmail.com>
2023-03-13 17:34:02 -0300

    create index.js
    Subindo com erro.

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-const COLUMN_NOT_ESPECIFIED = document.getElementById('not-Specified');
-const COLUMN_TO_DO = document.getElementById('toDo');
-const COLUMN_IN_PROGRESS = document.getElementById('in-Progress');
-const COLUMN_COMPLETED = document.getElementById('completed');
 
+let taskList = loadTasksFromLocalStorage(); // Carrega a lista de tarefas do armazenamento local
 const taskDefault = (id, task, statusTask) => ({id, task, statusTask,})  
+
+function init() {
+    // Criando novas Array apartir do taskList
+    let notSpecifiedList = taskList.filter(task => task.statusTask === 'not-Specified')
+    let toDoList = taskList.filter(task => task.statusTask === 'to-Do')
+    let inProgressList = taskList.filter(task => task.statusTask === 'in-Progress')
+    let completedList = taskList.filter(task => task.statusTask === 'completed')
+
+    notSpecifiedList.forEach(task => loadTaskInColumn('not-Specified', task.id));
+    toDoList.forEach(task => loadTaskInColumn('to-Do', task.id));
+    inProgressList.forEach(task => loadTaskInColumn('in-Progress', task.id));
+    completedList.forEach(task => loadTaskInColumn('completed', task.id));
+
+}
+
+init();
 
 /**
  *
@@ -24,43 +37,39 @@ function generateUniqueId() {
     );
 }
 
-function addTask(column) {
-
-    let taskList = loadTasksFromLocalStorage()
-    let uniqueID = generateUniqueId()
-    let newTask = taskDefault(uniqueID, "new task", column);
-    taskList.push(newTask)
-    saveTasksToLocalStorage(taskList);
-    loadTaskInColumn(column, uniqueID)
-
+function filterColumn(column) {
+    return column.slice(4); // remove o "btn-" do id da coluna encontrado.
 }
 
+function addTask(columnName) {
+    let uniqueID = generateUniqueId(); // Gera um ID único para a nova tarefa
+    let newColumn = filterColumn(columnName); // Filtra a coluna especificada
+    if (newColumn) { // Verifica se a coluna existe
+        let newTask = taskDefault(uniqueID, "new task", newColumn);
+        taskList.push(newTask); // Adiciona a nova tarefa à lista de tarefas
+        loadTaskInColumn(newColumn, uniqueID); // Carrega a nova tarefa na coluna correspondente
+        saveTasksToLocalStorage(taskList); // Salva a lista de tarefas atualizada no armazenamento local
+    }
+}
 function loadTaskInColumn(column, idTask) {
-    let taskList = loadTasksFromLocalStorage();
-
-    console.log(column, idTask)
+    
     const columnDiv = document.getElementById(column);
-    console.log(columnDiv)
     const matchingTask = taskList.find(task => task.id === idTask);
-
-    console.log(matchingTask)
-
-    const newDiv = createTaskCard(matchingTask);
-    columnDiv.appendChild(newDiv);
-
-    // const newDiv = document.createElement('div');
-    // newDiv.classList.add('card-task', 'rounded-2', 'shadow-sm');
-    // newDiv.setAttribute('id', 'cardTask');
-    // newDiv.innerHTML = `
-    // <span class="me-2">${matchingTask.task}</span>
-    // <button onclick="deleteTask(${idTask})" class="btn border-0"><i class="bi bi-x-circle-fill"></i></button>
-    // `;
-
-    // columnDiv.innerHTML += newDiv;
-
-    // return newDiv;
-
+    
+    if (!matchingTask) {
+        console.error(`Tarefa com ID ${idTask} não encontrada na lista.`);
+        return;
+    }
+    
+    try {
+        const newDiv = createTaskCard(matchingTask);
+        columnDiv.appendChild(newDiv);
+    } catch (error) {
+        console.error(`Erro ao criar cartão de tarefa: ${error.message}`);
+    }
+    
 }
+
 
 
 
@@ -70,9 +79,15 @@ function createTaskCard(task) {
     newDiv.setAttribute('id', 'cardTask');
     
     newDiv.innerHTML = `
-        <span class="me-2">${task.task}</span>
-        <button onclick="deleteTask(${task.id})" class="btn border-0"><i class="bi bi-x-circle-fill"></i></button>
-      `;
-      
+    <span class="me-2">${task.task}</span>
+    <button onclick="deleteTask(${task.id})" class="btn border-0"><i class="bi bi-x-circle-fill"></i></button>
+    `;
+    
     return newDiv;
-  }
+}
+
+window.addEventListener('storage', function(e) {
+    if (e.key === 'taskList') {
+        taskList = loadTasksFromLocalStorage();
+    }
+});
--- script.js
+++ script.js
@@ -1,68 +1,93 @@
-const buttons = document.querySelectorAll('#add-Task');
-const tasks = loadTasksFromLocalStorage();
-const deleteTask = document.createElement('button');
 
-deleteTask.id = 'deleteTask';
-deleteTask.classList.add('btn', 'border-0');
-deleteTask.innerHTML = '<i class="bi bi-x-circle-fill"></i>';
+let taskList = loadTasksFromLocalStorage(); // Carrega a lista de tarefas do armazenamento local
+const taskDefault = (id, task, statusTask) => ({id, task, statusTask,})  
 
-buttons.forEach(button => {
-  button.addEventListener('click', () => {
-    const column = button.parentElement.parentElement;
-    const status = column.id.slice(7); // remove o "column-" do id encontrado.
-    const newDiv = document.createElement('div');
+function init() {
+    // Criando novas Array apartir do taskList
+    let notSpecifiedList = taskList.filter(task => task.statusTask === 'not-Specified')
+    let toDoList = taskList.filter(task => task.statusTask === 'to-Do')
+    let inProgressList = taskList.filter(task => task.statusTask === 'in-Progress')
+    let completedList = taskList.filter(task => task.statusTask === 'completed')
 
-    newDiv.classList.add('cardTask', 'rounded-2', 'shadow-sm');
-    newDiv.setAttribute('id', 'cardTask');
-    newDiv.insertAdjacentHTML('beforeend', '<span class="me-2">Empty the little box</span>');
+    notSpecifiedList.forEach(task => loadTaskInColumn('not-Specified', task.id));
+    toDoList.forEach(task => loadTaskInColumn('to-Do', task.id));
+    inProgressList.forEach(task => loadTaskInColumn('in-Progress', task.id));
+    completedList.forEach(task => loadTaskInColumn('completed', task.id));
 
-    const deleteTaskClone = deleteTask.cloneNode(true);
-    deleteTaskClone.addEventListener('click', () => {
-      window.location.reload()
-      newDiv.remove();
-      const spanText = newDiv.querySelector('span').textContent;
-      excluirDoLocalStorage(spanText);
-    });
+}
 
-    newDiv.appendChild(deleteTaskClone);
-    column.appendChild(newDiv);
+init();
 
-    const newTask = {
-      id: generateUniqueId(),
-      task: "Empty the little box",
-      status,
+/**
+ *
+ */
+
+function saveTasksToLocalStorage(tasks) {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function loadTasksFromLocalStorage() {
+    const tasksJSON = localStorage.getItem('tasks');
+    return tasksJSON ? JSON.parse(tasksJSON) : [];
+}
+
+function generateUniqueId() {
+    return (
+        Math.random().toString(16).substr(2, 8)
+    );
+}
+
+function filterColumn(column) {
+    return column.slice(4); // remove o "btn-" do id da coluna encontrado.
+}
+
+function addTask(columnName) {
+    let uniqueID = generateUniqueId(); // Gera um ID único para a nova tarefa
+    let newColumn = filterColumn(columnName); // Filtra a coluna especificada
+    if (newColumn) { // Verifica se a coluna existe
+        let newTask = taskDefault(uniqueID, "new task", newColumn);
+        taskList.push(newTask); // Adiciona a nova tarefa à lista de tarefas
+        loadTaskInColumn(newColumn, uniqueID); // Carrega a nova tarefa na coluna correspondente
+        saveTasksToLocalStorage(taskList); // Salva a lista de tarefas atualizada no armazenamento local
+    }
+}
+function loadTaskInColumn(column, idTask) {
+    
+    const columnDiv = document.getElementById(column);
+    const matchingTask = taskList.find(task => task.id === idTask);
+    
+    if (!matchingTask) {
+        console.error(`Tarefa com ID ${idTask} não encontrada na lista.`);
+        return;
     }
     
-    tasks.push(newTask);
-    saveTasksToLocalStorage(tasks)
+    try {
+        const newDiv = createTaskCard(matchingTask);
+        columnDiv.appendChild(newDiv);
+    } catch (error) {
+        console.error(`Erro ao criar cartão de tarefa: ${error.message}`);
+    }
+    
+}
 
-  });
+
+
+
+function createTaskCard(task) {
+    const newDiv = document.createElement('div');
+    newDiv.classList.add('card-task', 'rounded-2', 'shadow-sm');
+    newDiv.setAttribute('id', 'cardTask');
+    
+    newDiv.innerHTML = `
+    <span class="me-2">${task.task}</span>
+    <button onclick="deleteTask(${task.id})" class="btn border-0"><i class="bi bi-x-circle-fill"></i></button>
+    `;
+    
+    return newDiv;
+}
+
+window.addEventListener('storage', function(e) {
+    if (e.key === 'taskList') {
+        taskList = loadTasksFromLocalStorage();
+    }
 });
-
-function distributeTasksByColumnFromLocalStorage() {
-
-  const tasks = loadTasksFromLocalStorage();
-  const tasksNoSpecified = tasks.filter(task => task.status === 'Not-Specified');
-  const tasksToDo = tasks.filter(task => task.status === 'To-Do');
-  const tasksInProgress = tasks.filter(task => task.status === 'In-Progress');
-  const tasksCompleted = tasks.filter(task => task.status === 'Completed');
-  return [tasksNoSpecified, tasksToDo, tasksInProgress, tasksCompleted];
-
-}
-
-const [tasksNoSpecified, tasksToDo, tasksInProgress, tasksCompleted] = distributeTasksByColumnFromLocalStorage();
-
-const taskLists = {
-  'column-Not-Specified': tasksNoSpecified,
-  'column-To-Do': tasksToDo,
-  'column-In-Progress': tasksInProgress,
-  'column-Completed': tasksCompleted
-};
-
-for (const [listId, tasks] of Object.entries(taskLists)) {
-  const taskList = document.querySelector(`#${listId}`);
-  tasks.forEach(task => {
-    const newDiv = createTaskCard(task);
-    taskList.appendChild(newDiv);
-  });
-}

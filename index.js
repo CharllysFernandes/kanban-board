29e9d8410c31fddd8d1db41f4595a9e8a343f7b9
Charllys Fernandes <charllys.daiany@gmail.com>
2023-03-15 18:02:30 -0300

    update index.js
    Initialize create implements sortable.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
+const taskDefault = (id, task, statusTask) => ({ id, task, statusTask });
 let taskList = loadTasksFromLocalStorage()
-const taskDefault = (id, task, statusTask) => ({id, task, statusTask});
 
 function loadTasksFromLocalStorage() {
     const tasksJSON = localStorage.getItem('tasks')
@@ -17,32 +17,33 @@ function generateUniqueId() {
     )
 }
 
-function deleteTask(params) {
-    console.log(params)
+function filterColumn(idColumn) {
+    return idColumn.slice(4);
 }
+function addNewTask(column, taskName = "New Task") {
+    const newColumn = filterColumn(column);
 
-function filterColumn(params) {
-    return params.slice(4);
-}
-
-function addNewTask(params) {
-    let uniqueID = generateUniqueId();
-    let newColumn = filterColumn(params);
-    
-    if (newColumn) {
-        let newTask = taskDefault(uniqueID, "New Task", newColumn)
-        taskList.push(newTask)
-        
-        loadTaskInColumn(newColumn, uniqueID)
-        saveTasksToLocalStorage(taskList)
+    if (!newColumn) {
+        console.error(`Coluna inválida: ${column}`);
+        return null;
     }
+
+    const uniqueID = generateUniqueId();
+    const newTask = taskDefault(uniqueID, taskName, newColumn);
+
+    taskList.push(newTask);
+    loadTaskInColumn(newColumn, uniqueID);
+    saveTasksToLocalStorage(taskList);
+
+    return newTask;
 }
+
 
 function loadTaskInColumn(column, idTask) {
-    
+
     const columnDiv = document.getElementById(column);
     const matchingTask = taskList.find(task => task.id === idTask)
-    
+
     if (!matchingTask) {
         console.error(`Tarefa com ID ${idTask} não encontrada na lista`)
         return;
@@ -51,24 +52,47 @@ function loadTaskInColumn(column, idTask) {
     try {
         const newDiv = createTaskCard(matchingTask)
         columnDiv.appendChild(newDiv)
-        
+        addNewButtons()
+        addEditTask()
+
     } catch (error) {
         console.error(`Erro ao criar cartão de tarefa: `)
-        
+
     }
+    
 }
 
-function createTaskCard(params) {
+// function addEditTask() {
+//     const taskSpan = newDiv.querySelector('.card-task');
+//     taskSpan.addEventListener('click', editTask);
+// }
+
+function createTaskCard({ id, task }) {
     const newDiv = document.createElement('div');
-    newDiv.classList.add('card-task', 'rounded-2', 'shadow-sm')
-    newDiv.setAttribute('id', 'cardTask')
-    
+    newDiv.classList.add('card-task', 'rounded-2', 'shadow-sm');
+    newDiv.setAttribute('data-task-id', id);
+    newDiv.setAttribute('id', id);
+
     newDiv.innerHTML = `
-    <span class="me-2">${params.task}</span>
-    <button onclick="deleteTaskById(${params.id})" class="btn border-0"><i class="bi bi-x-circle-fill"></i></button>
-    `
-    
-    return newDiv
+        <span class="me-2">${task}</span>
+        <button id="btnDeleteTask" class="btn border-0 m-0 p-0"><i class="bi bi-x-circle-fill"></i></button>
+    `;
+
+    return newDiv;
+}
+
+function addEditTask() {
+    const taskDiv = document.querySelectorAll('.card-task');
+    taskDiv.forEach(event => {
+        event.addEventListener('click', editTask)
+    })
+}
+
+function addNewButtons() {
+    const deleteButtons = document.querySelectorAll('#btnDeleteTask');
+    deleteButtons.forEach(button => {
+        button.addEventListener('click', deleteTask);
+    });
 }
 
 function init() {
@@ -93,36 +117,27 @@ function init() {
         });
     }
 }
-function deleteTaskById(taskID) {
-    const taskIdString = `${taskID}`;
-    const taskIndex = taskList.findIndex(task => task.id === taskIdString);
-    
-    if (taskIndex === -1) {
-        console.error(`Tarefa com ID ${taskID} não encontrada na lista`);
+
+function deleteTask(event) {
+    const taskCard = event.target.closest('.card-task');
+    if (!taskCard) {
         return;
     }
 
-    taskList.splice(taskIndex, 1);
-    saveTasksToLocalStorage(taskList);
+    const taskId = taskCard.getAttribute('data-task-id');
+    const taskIndex = taskList.findIndex(task => task.id === taskId);
 
-    const taskColumn = taskDefault.statusColumn;
-    const taskCard = document.getElementById(`cardTask_${taskIdString}`);
-
-    if (taskCard) {
-        taskCard.remove();
-    }
-
-    console.log(`Tarefa com ID ${taskID} removida da lista`);
-
-    if (taskColumn) {
-        const columnDiv = document.getElementById(taskColumn);
-        if (columnDiv) {
-            columnDiv.removeChild(taskCard);
-            console.log(`Tarefa removida da coluna ${taskColumn}`);
-        }
+    if (taskIndex !== -1) {
+        taskList.splice(taskIndex, 1);
+        removeTaskCard(taskCard);
+        saveTasksToLocalStorage(taskList);
     }
 }
 
+function removeTaskCard(taskCard) {
+    const columnDiv = taskCard.parentNode;
+    columnDiv.removeChild(taskCard);
+}
 
 
 window.addEventListener('storage', function (e) {
@@ -131,6 +146,43 @@ window.addEventListener('storage', function (e) {
     }
 })
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     init();
+
+});
+
+function editTask(event) {
+    const taskSpan = event.target;
+    const taskId = taskSpan.parentNode.getAttribute('data-task-id');
+    const taskIndex = taskList.findIndex(task => task.id === taskId);
+
+    if (taskIndex !== -1) {
+        const newTaskName = prompt('Digite o novo nome da tarefa:', taskList[taskIndex].task);
+        if (newTaskName) {
+            taskList[taskIndex].task = newTaskName;
+            taskSpan.textContent = newTaskName;
+            saveTasksToLocalStorage(taskList);
+        }
+    }
+}
+
+// Selecione as colunas da sua lista de tarefas
+const columns = document.querySelectorAll('.task-column');
+
+// Para cada coluna, crie uma instância do Sortable
+columns.forEach(column => {
+  new Sortable(column, {
+    group: 'tasks', // Agrupa as colunas da lista de tarefas
+    animation: 150, // Define a velocidade da animação de arrastar e soltar
+    onEnd: () => { // Quando a tarefa for solta em outra coluna
+      // Percorra todas as tarefas e atualize o status
+      taskList.forEach(task => {
+        const taskCard = document.getElementById(task.id);
+        const columnId = taskCard.parentNode.id;
+        const newStatus = filterColumn(columnId);
+        task.statusTask = newStatus;
+      });
+      saveTasksToLocalStorage(taskList);
+    }
+  });
 });
